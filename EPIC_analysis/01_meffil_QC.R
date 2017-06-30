f9c6f791639660cac8ace3fd3af96b50d4067cef
asaffa <ayden.saffari@gmail.com>
2017-06-30 13:10:28 +0100

    1. removed meffil - easier to just get this from the main repo, 2. changes to 01_meffil_QC.R

--- EPIC_analysis/01_meffil_QC.R
+++ EPIC_analysis/01_meffil_QC.R
@@ -54,19 +54,6 @@ options(mc.cores=ncores)
 sample_sheet <- meffil.read.samplesheet(base=args[1],recursive=T,
 					ignore.case=T,verbose=T)
 
-#sometimes samplesheet function does not guess filenames correctly, 
-#if so, use list of idats from basenames function (seems to be more reliable)
-if(any(sample_sheet$Basename == "character(0)")){
-    sample_sheet$Basename <- as.character(sample_sheet$Basename)
-    idats <- meffil.basenames(args[1], recursive = T)
-    idats <- data.frame(barcode=basename(idats),Basename=idats)
-    idats$Basename <- as.character(idats$Basename)
-    sample_sheet$Basename[sample_sheet$barcode %in% idats$barcode] <- 
-    idats[idats$barcode %in% sample_sheet$barcode,2]
-}
-
-sample_sheet <- sample_sheet[-which(sample_sheet$Basename == "character(0)"),]	
-
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # create QC objects, perform background correction, dye bias correction, sex prediction,
 # cell count estimates
